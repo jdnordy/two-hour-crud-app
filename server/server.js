5aa640b9c16367bb9ef5ab4ac2039c13c10642f6
Joshua Nordstrom <jdnordy@gmail.com>
2020-02-28 16:12:48 -0800

    set up basic server

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 3000;
+
+
+
+
+
+app.use((req, res) => {
+  res.sendStatus(404);
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  console.log(`Server error: ${err}`);
+  res.status(400).json(`Server error: ${err}`);
+});
+
+
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
